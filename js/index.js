3cfdd86b2af7bef7bd4e1308a109d52e86dd870f
Tijn Kersjes <tijn@divbyzero.nl>
2016-12-30 01:54:25 +0100

    Created small demo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-import isomer from "isomer";
+import Isomer, { Shape, Point, Color } from "isomer";
 
-console.log(isomer);
+const red = new Color(160, 60, 50);
+const blue = new Color(50, 60, 160);
+
+const init = () => {
+
+    const canvas = document.getElementById("playground");
+    const iso = new Isomer(canvas);
+
+    iso.add(Shape.Prism(Point.ORIGIN, 3, 3, 1));
+    iso.add(Shape.Pyramid(Point(0, 2, 1)), red);
+    iso.add(Shape.Prism(Point(2, 0, 1)), blue);
+};
+
+window.addEventListener("load", init, false);
